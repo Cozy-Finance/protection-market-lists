989340c912015ffdec18291da2a69587e87f9386
Matt Solomon <matt@mattsolomon.dev>
2021-09-10 16:07:16 -0700

    test: more thorough protection market list example
    casting format was changed in types.test.ts because the old format would error because
    it couldn't assign a string to an enum, but the new format can handle that -- to verify
    the new format is ok, just change the example list to not conform to the schema and
    the tests should fail

--- test/types.test.ts
+++ test/types.test.ts
@@ -4,7 +4,7 @@ import exampleList from './schema/example.protectionmarketlist.json';
 describe('types', () => {
   it('matches example schema', () => {
     // this is enough--typescript won't cast it unless it matches the interface
-    const list: ProtectionMarketList = exampleList;
+    const list = exampleList as ProtectionMarketList;
 
     expect(list.name).toEqual('My Protection Market List');
   });
