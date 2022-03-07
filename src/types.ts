f2f3b1ac416c8d431a5bf0bd5a92dd1aae2e3dd7
Dave Pekar <dpekar@gmail.com>
2022-03-07 11:40:28 -0700

    Updated types with new self-serve schema

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,3 @@
-export interface TokenInfo {
-  readonly chainId: number;
-  readonly address: string;
-  readonly name: string;
-  readonly decimals: number;
-  readonly symbol: string;
-  readonly logoURI?: string;
-  readonly tags?: string[];
-  readonly extensions?: {
-    readonly [key: string]: string | number | boolean | null;
-  };
-}
-
 export interface Version {
   readonly major: number;
   readonly minor: number;
@@ -24,19 +11,32 @@ export interface Tags {
   };
 }
 
+export interface InvestmentOpportunity {
+  readonly divest: DivestScript;
+  readonly dataURI: string; // API endpoint to query for user data from, e.g. balances
+  readonly invest: InvestScript;
+  readonly logoURI?: string;
+  readonly strategy?: StrategyLeg[];
+  readonly tokens: {
+    // arrays of token addresses, to be looked up in the `tokens` mapping
+    readonly input: string[]; // token addresses the user will deposit to enter this opportunity, and receipt upon exiting
+    readonly receipt: string[]; // receipt token addresses the user needs to exit this opportunity
+    readonly rewards: string[]; // rewards token addresses the user may claim when exiting this opportunity
+  };
+}
+
 // Parameters for investing into a protected investment.
 //
-// The function signature is constant, which is why it's an optional property. The `tokens` property defines the
-// set of input tokens that can be used to deposit into this investment opportunity. The items in the function
-// signature correspond to these `tokens`. For example, if we're investing in the 3Crypto pool, the `tokens` array
-// would be [ETH, WBTC, USDT]. When calling `invest` on `address`, amounts[0] would correspond to the amount of ETH
-// to invest, amounts[1] would correspond to the amount of WBTC to invest, etc. An amount of MAX_UINT256 should be
-// used to signal that you want to invest all tokens you have
+// Example  'function invest(uint256[] calldata amounts) external payable'
+//
+//  The `tokens` property defines the  set of input tokens that can be used to deposit into this investment opportunity.
+// The items in the function signature correspond to these `tokens`. For example, if we're investing in the 3Crypto pool,
+// the `tokens` array would be [ETH, WBTC, USDT]. When calling `invest` on `address`, amounts[0] would correspond to the
+// amount of ETH to invest, amounts[1] would correspond to the amount of WBTC to invest, etc. An amount of MAX_UINT256
+// should be used to signal that you want to invest all tokens you have
 export interface InvestScript {
   readonly address: string;
-  readonly signature:
-    | 'function invest(uint256[] calldata amounts) external payable'
-    | 'function invest(uint256[] calldata amounts, bytes calldata data) external payable';
+  readonly signature: string;
 }
 
 // Parameters for exiting a protected investment.
@@ -47,44 +47,43 @@ export interface InvestScript {
 // tokens and reward tokens should be sent to
 export interface DivestScript {
   readonly address: string;
-  readonly signature:
-    | 'function divest(address recipient, uint256[] calldata receiptAmounts, uint256[] calldata rewardAmounts) external payable'
-    | 'function divest(address recipient, uint256[] calldata receiptAmounts, uint256[] calldata rewardAmounts, bytes calldata data) external payable';
+  readonly signature: string;
 }
 
 export interface StrategyLeg {
-  action: 'Borrow' | 'Swap to' | 'Deposit for' | 'Deposit to' | 'Stake on';
-  token: string; // address of the token, to be looked up in the `tokens` mapping
-  platformId: number; // Platform ID of strategy target
-  logoURI?: string;
-}
-
-export interface InvestmentOpportunity {
-  readonly invest: InvestScript;
-  readonly divest: DivestScript;
-  readonly dataURI: string; // API endpoint to query for user data from, e.g. balances
-  readonly tokens: {
-    // arrays of token addresses, to be looked up in the `tokens` mapping
-    readonly input: string[]; // token addresses the user will deposit to enter this opportunity, and receipt upon exiting
-    readonly receipt: string[]; // receipt token addresses the user needs to exit this opportunity
-    readonly rewards: string[]; // rewards token addresses the user may claim when exiting this opportunity
-  };
-  readonly strategy?: StrategyLeg[];
-}
-
-export interface MarketInfo extends TokenInfo {
-  readonly underlying: string; // address of underlying, if `underlying === 0xEeee....EEeE`, underlying is ETH
-  readonly trigger: string; // trigger contract address for protection markets, or zero address for money markets
-  readonly investmentOpportunity?: InvestmentOpportunity; // details on how protected investing using this market
+  readonly action: 'Borrow' | 'Swap to' | 'Deposit for' | 'Deposit to' | 'Stake on';
+  readonly actionReceiver: string;
+  readonly logoURI?: string;
+  readonly platformId: number; // Platform ID of strategy target
+  readonly token: string; // address of the token, to be looked up in the `tokens` mapping
 }
 
 export interface MarketList {
-  readonly name: string;
-  readonly timestamp: string;
-  readonly version: Version;
-  readonly markets: MarketInfo[];
-  readonly tokens: Record<string, TokenInfo>; // maps from checksummed token address to TokenInfo
   readonly keywords?: string[];
-  readonly tags?: Tags;
   readonly logoURI?: string;
+  readonly markets: MarketInfo[];
+  readonly name: string;
+  readonly tags?: Tags;
+  readonly timestamp: string;
+  readonly tokens: Record<string, TokenInfo>; // maps from checksummed token address to TokenInfo
+  readonly version: Version;
+}
+
+export interface MarketInfo extends TokenInfo {
+  readonly investmentOpportunity?: InvestmentOpportunity; // details on how protected investing using this market
+  readonly trigger: string; // trigger contract address for protection markets, or zero address for money markets
+  readonly underlying: string; // address of underlying, if `underlying === 0xEeee....EEeE`, underlying is ETH
+}
+
+export interface TokenInfo {
+  readonly address: string;
+  readonly chainId: number;
+  readonly decimals: number;
+  readonly extensions?: {
+    readonly [key: string]: string | number | boolean | null;
+  };
+  readonly logoURI?: string;
+  readonly name: string;
+  readonly symbol: string;
+  readonly tags?: string[];
 }
